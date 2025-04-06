9d3291cfa0d90cb328dafa9ed1f587579ccc38bf
Izzat Zamri <despairboy.izzat@gmail.com>
2025-04-07 03:27:28 +0800

    update index page

--- pages/worlds/[world_id]/characters/index.vue
+++ pages/worlds/[world_id]/characters/index.vue
@@ -3,13 +3,17 @@
     <h1>Character Lists</h1>
 
     <div>
-      <NuxtLink to="/worlds/world-name/characters/new">
+      <NuxtLink :to="`/worlds/${worldId}/characters/new`">
         + Create New Character
       </NuxtLink>
     </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const worldId = route.params.world_id
+</script>
 
 <style scoped lang="scss"></style>
