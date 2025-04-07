8fb93a8b98f96c9b5c13b28db6f86ea78b255cee
Izzat Zamri <despairboy.izzat@gmail.com>
2025-04-08 02:38:20 +0800

    use primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,29 @@
 // nuxt.config.ts
 import { defineNuxtConfig } from 'nuxt/config'
+import Aura from '@primeuix/themes/aura'
 
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt'],
-
-  css: ['~/assets/styles/main.scss'],
+  modules: ['@pinia/nuxt', '@primevue/nuxt-module'],
+  primevue: {
+    options: {
+      ripple: true,
+      theme: {
+        preset: Aura, // Use your desired theme preset from @primeuix/themes
+        options: {
+          // prefix: 'p', // This lets you use <p-button> instead of <Button>
+          darkModeSelector: 'system',
+          cssLayer: false,
+        },
+      },
+    },
+    components: {
+      include: ['Button', 'InputText'],
+    },
+  },
+  css: [
+    '~/assets/styles/main.scss',
+    'primeicons/primeicons.css', // Include icon styles
+  ],
   compatibilityDate: '2025-03-09',
   router: {},
 })
